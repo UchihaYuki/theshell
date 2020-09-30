990c9b6eb0027549689ec9dc394cca76f7885dab
UchihaYuki <uchiha_yuki@live.com>
2020-09-30 13:27:03 +0800

    2020-09-30T05:27:03.386Z

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,15 @@ const del = require("del");
 const mocha = require("gulp-mocha");
 const sourcemaps = require('gulp-sourcemaps')
 const ts = require('gulp-typescript');
-const { exec } = require('child_process');
+const argv = require('yargs').argv
+const fs = require('fs')
+const shell = require('shelljs');
 
 const src = ts.createProject("src/tsconfig.json");
 const srcRelease = ts.createProject("src/tsconfig-release.json");
 const test = ts.createProject("test/tsconfig.json");
+
+const starter = "node-lib-starter";
 
 gulp.task("clean", () => del([
     "lib",
@@ -43,11 +47,49 @@ gulp.task("build:src-release", () => gulp
     .pipe(srcRelease())
     .pipe(gulp.dest("lib")));
 
-gulp.task("git-push", (cb) => {
-    exec("push.cmd", cb)
+gulp.task('push', function (cb) {
+    // if (!argv.m) {
+    //     return cb(new Error("usage: gulp push -m <message>"))
+    // }
+    const message = argv.m || new Date().toISOString()
+
+    let index = __dirname.lastIndexOf("/");
+    if (index == -1) index = __dirname.lastIndexOf("\\");
+    const parentDir = __dirname.slice(index + 1);
+    if (parentDir != starter && !fs.existsSync('.init')) {
+        return cb(new Error("project has not been initialized, run 'gulp init' to initialize project"))
+    }
+
+    shell.exec(`git add .`)
+    shell.exec(`git commit -m ${message}`)
+    shell.exec(`git push`)
+    cb();
+})
+
+gulp.task('init', function (cb) {
+    if (!argv.r) {
+        return cb(new Error("usage: gulp init -r <repo>"))
+    }
+    
+    let index = __dirname.lastIndexOf("/");
+    if (index == -1) index = __dirname.lastIndexOf("\\");
+    const parentDir = __dirname.slice(index + 1);
+    if (parentDir == starter) {
+        return cb(new Error(`${starter} doesn't need to be initialized`))        
+    }
+
+    if (fs.existsSync('.init')) {
+        return cb(new Error("project has already been initialized"))
+    }
+
+    shell.exec(`git remote set-url origin ${argv.r}`);
+    shell.exec(`touch .init`)
+    shell.exec(`git add .`)
+    shell.exec(`git commit -m init`)
+    shell.exec(`git push -u origin master`)
+    cb();
 })
 
 gulp.task("debug:test", gulp.series("clean", "build:src", "build:test"));
 gulp.task("test", gulp.series("clean", "build:src", "build:test", "run:test"));
-gulp.task("prepublish", gulp.series("test", "build:src-release", "git-push"));
-
+gulp.task("prepublish", gulp.series("test", "build:src-release", "push"));
