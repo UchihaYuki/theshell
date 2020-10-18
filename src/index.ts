e448836ff636c958d7ced06249b5a8cd69e81477
UchihaYuki <uchiha_yuki@live.com>
2020-10-18 17:33:32 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-import process from 'process';
+import { spawn, spawnSync, SpawnOptions } from 'child_process';
+
+export type ShellCallback = (code: number | null, signal: NodeJS.Signals | null) => void;
+export function shell(command: string): void
+export function shell(command: string, cb: ShellCallback): void
+export function shell(command: string, options: SpawnOptions): void
+export function shell(command: string, options: SpawnOptions, cb: ShellCallback): void
+export function shell(command: string, cb_options?: SpawnOptions | ShellCallback, cb?: ShellCallback) {
+    const so = {
+        shell: true,
+        stdio: 'inherit'
+    } as SpawnOptions;
+
+    if (!cb_options) {
+        return spawnSync(command, so);
+    }
+
+    if (!cb) {
+        if (typeof cb_options == 'object') {
+            Object.assign(so, cb_options)
+            return spawnSync(command, so)
+        }
+
+        const child = spawn(command, {
+            shell: true,
+            stdio: 'inherit'
+        });
+        child.on('exit', cb_options)
+        return;
+    }
+
+    Object.assign(so, cb_options)
+    const child = spawn(command, so);
+    child.on('exit', cb)
+}
--- test/test.ts
+++ test/test.ts
@@ -1,19 +1,31 @@
 import { assert } from 'chai';
+import { shell } from '../src/index';
 
-describe("", function() {
+describe("shell", function() {
 
-    it("", function() {
+    it("function shell(command: string): void", function() {
+       shell('docker image list | grep juju')
+    });
+    
+    it("shell(command: string, cb: ShellCallback): void", function(done) {
+       shell('docker image list | grep juju', done)
+    });
+    
+    it("function shell(command: string, options: SpawnOptions): void", function() {
+       const r = shell('env | grep LANGS', {
+           env: {
+               LANGS: "en zh"
+           }
+       })
+    });
+    
+    it("function shell(command: string, options: SpawnOptions, cb: ShellCallback): void", function(done) {
+       const r = shell('env | grep LANGS', {
+           env: {
+               LANGS: "en zh"
+           }
+       }, done)
     });
 
-});
-
-describe("", function() {
-
-    describe("", function () {
-
-        it("", function() {
-        });
-
-    })
 
 });
