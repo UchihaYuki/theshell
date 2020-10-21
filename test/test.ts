83de5c966f38c7f6bbc02d43301ad9b4c4cd147c
UchihaYuki <uchiha_yuki@live.com>
2020-10-21 13:33:57 +0800

    2020-10-21T05:33:57.439Z

--- test/test.ts
+++ test/test.ts
@@ -1,40 +1,43 @@
 import { assert } from 'chai';
 import { shell, shellAsync } from '../src/index';
 
-describe("shell", function() {
+// describe("shell", function () {
 
-    it("function shell(command: string): void", function() {
-       shell('docker image list | grep juju')
-    });
-    
-    it("shell(command: string, cb: ShellCallback): void", function(done) {
-       shell('docker image list | grep juju', done)
-    });
-    
-    it("function shell(command: string, options: SpawnOptions): void", function() {
-       const r = shell('env | grep LANGS', {
-           env: {
-               LANGS: "en zh"
-           }
-       })
-    });
-    
-    it("function shell(command: string, options: SpawnOptions, cb: ShellCallback): void", function(done) {
-       const r = shell('env | grep LANGS', {
-           env: {
-               LANGS: "en zh"
-           }
-       }, done)
-    });
+//     it("function shell(command: string): void", function () {
+//         shell('docker image list | grep juju')
+//     });
 
+//     it("shell(command: string, cb: ShellCallback): void", function (done) {
+//         shell('docker image list | grep juju', done)
+//     });
 
-});
+//     it("function shell(command: string, options: SpawnOptions): void", function () {
+//         const r = shell('env | grep LANGS', {
+//             env: {
+//                 LANGS: "en zh"
+//             }
+//         })
+//     });
 
-describe("Async", function() {
+//     it("function shell(command: string, options: SpawnOptions, cb: ShellCallback): void", function (done) {
+//         const r = shell('env | grep LANGS', {
+//             env: {
+//                 LANGS: "en zh"
+//             }
+//         }, done)
+//     });
+// });
 
-    it("function shellAsync(command: string, options?: SpawnOptions)", () => shellAsync('bash -c "ls"'));
-    
-    it("function shellAsync(command: string, options?: SpawnOptions)", () => shellAsync('bash -c "ls"', {
-        cwd: "src"
-    }));
+describe("Async", function () {
+
+    // it("function shellAsync(command: string, options?: SpawnOptions)", () => shellAsync('bash -c "ls"'));
+
+    // it("function shellAsync(command: string, options?: SpawnOptions)", () => shellAsync('bash -c "ls"', {
+    //     cwd: "src"
+    // }));
+
+    it("chain function shellAsync(command: string, options?: SpawnOptions)", () =>
+        shellAsync(`ls`)
+            .then(() => shellAsync(`ls src`))
+            .then(() => shellAsync('ls test')));
 });
